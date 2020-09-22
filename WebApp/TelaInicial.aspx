8043f9295ee21a33be2441cc3bd994b479366f57
Gabriel Harmony <gabrielharmony@hotmail.com>
2020-09-21 22:48:07 -0300

    Desenvolvimento da página Tela Inicial

---
+++ WebApp/TelaInicial.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Principal.Master" CodeBehind="TelaInicial.aspx.vb" Inherits="WebApp.TelaInicial" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>Início</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <div id="organizar">
+            <nav class="nav justify-content-end">
+                <asp:Label ID="Label1" runat="server" Text="Organizar por: "></asp:Label>
+                 &nbsp;
+                <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+            </nav>
+        </div>
+        <br />
+    </div>
+    <div id="pacientes">
+        <div class="btn" role="button">
+            <div class="card text-center btnpaciente">
+                <asp:ImageButton ID="ImageButton1" runat="server" CssClass="pacnteimg" BorderColor="#12DAFA" ImageUrl="~/img/paciente.png"/>
+                <div class="card-body">
+                    <asp:Label ID="Label2" runat="server" Text="nomePaciente"></asp:Label> 
+                    <br />
+                    <asp:Label ID="Label3" runat="server" Text="Tratamento: "></asp:Label>
+                    <asp:Label ID="Label4" runat="server" Text="(id/nome)"></asp:Label>
+                    <br />
+                    <asp:Label ID="Label5" runat="server" Text="Status: "></asp:Label>
+                    <asp:Label ID="Label6" runat="server" Text="Valor"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="btn" role="button">
+            <div class="card text-center btnpaciente">
+                <asp:ImageButton ID="ImageButton2" runat="server" CssClass="pacnteimg" BorderColor="#12DAFA" ImageUrl="~/img/paciente.png"/>
+                <div class="card-body">
+                    <asp:Label ID="Label7" runat="server" Text="nomePaciente"></asp:Label> 
+                    <br />
+                    <asp:Label ID="Label8" runat="server" Text="Tratamento: "></asp:Label>
+                    <asp:Label ID="Label9" runat="server" Text="(id/nome)"></asp:Label>
+                    <br />
+                    <asp:Label ID="Label10" runat="server" Text="Status: "></asp:Label>
+                    <asp:Label ID="Label11" runat="server" Text="Valor"></asp:Label>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="content_telainicial_buttons">
+        <asp:Button ID="Button1" runat="server" CssClass="btn btn-primary btn-lg" Widht="120px" Text="Cadastrar Paciente" />
+        <asp:Button ID="Button2" runat="server" CssClass="btn btn-primary btn-lg" Width="120px" Text="Perfil" />
+    </div>
+</asp:Content>
---
+++ WebApp/TelaInicial.aspx.designer.vb
@@ -0,0 +1,159 @@
+﻿'------------------------------------------------------------------------------
+' <gerado automaticamente>
+'     Este código foi gerado por uma ferramenta.
+'
+'     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+'     o código for recriado
+' </gerado automaticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class TelaInicial
+
+    '''<summary>
+    '''Controle Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList1 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle ImageButton1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents ImageButton1 As Global.System.Web.UI.WebControls.ImageButton
+
+    '''<summary>
+    '''Controle Label2.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label2 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label3.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label3 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label4.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label4 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label5.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label5 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label6.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label6 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle ImageButton2.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents ImageButton2 As Global.System.Web.UI.WebControls.ImageButton
+
+    '''<summary>
+    '''Controle Label7.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label7 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label8.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label8 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label9.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label9 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label10.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label10 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label11.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label11 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Button1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle Button2.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Button2 As Global.System.Web.UI.WebControls.Button
+End Class
