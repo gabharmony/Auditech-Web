6f2cf5be2ab10f3f627f38140046532471fd5e6e
Gabriel Harmony <gabrielharmony@hotmail.com>
2020-10-09 03:50:54 -0300

    Desenvolvimento do Layoult da pág Tratamento - Finalizado

--- WebApp/Tratamento.aspx
+++ WebApp/Tratamento.aspx
@@ -4,7 +4,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Tratamento</title>
     <link href="Content/bootstrap.min.css" rel="stylesheet" />
     <link href="Content/toastr.min.css" rel="stylesheet" />
@@ -19,30 +19,136 @@
     <form id="form1" runat="server">
         <div>
             <div id="header">
-                <nav class="navbar navbar-expand-lg navbar-light" >
+                <nav class="navbar navbar-expand-lg navbar-light">
                     <a class="navbar-brand" href="#">
                         <img src="./img/Logo_02.jpeg" class="rounded" width="100" height="100" alt="" loading="lazy" />
                     </a>
-                    <div class="row">
-                        <div class="col-6 col-sm-auto">
-                            <asp:Label ID="Label1" runat="server" Text="ID: "></asp:Label>
-                             &nbsp;
-                            <asp:TextBox ID="TextBox1" runat="server" placeholder="Digite o ID"></asp:TextBox>
-                            <br /><br />
-                            <asp:Label ID="Label2" runat="server" Text="PACIENTE: "></asp:Label>
-                             &nbsp;
-                            <asp:Label ID="Label3" runat="server" Text="Prenome / Nome Sobrenome"></asp:Label>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <div class="row">
+                                <div class="col-6">
+                                    <asp:Label ID="Label1" runat="server" Font-Bold="true" Text="ID: "></asp:Label>
+                                    &nbsp;
+                                    <asp:TextBox ID="TextBox1" runat="server" placeholder="Digite o ID"></asp:TextBox>
+                                </div>
+                                <div class="col-6 col-sm-auto">
+                                    <asp:Label ID="Label4" runat="server" Font-Bold="true" Text="STATUS: "></asp:Label>
+                                    &nbsp;
+                                    <asp:Label ID="Label5" runat="server" Text="ATIVO / REPOUSO / INATIVO "></asp:Label>
+                                </div>
+                                <br /><br />
+                                <div class="col-6">
+                                    <asp:Label ID="Label2" runat="server" Font-Bold="true" Text="PACIENTE: "></asp:Label>
+                                    &nbsp;
+                                    <asp:Label ID="Label3" runat="server" Text="Prenome / Nome Sobrenome"></asp:Label>
+                                </div>
+                                <div class="col-6 col-sm-auto">
+                                    <asp:Label ID="Label6" runat="server" Font-Bold="true" Text="DATA NASCIMENTO: "></asp:Label>
+                                    &nbsp;
+                                    <asp:Label ID="Label7" runat="server" Text="DD/MM/AAAA"></asp:Label>
+                                </div>
+                            </div>
                         </div>
-                        <br />
-                        <div class="col-6 col-sm-auto">
-          
+                        <div class="form-inline">
+                            <asp:Button ID="btnUsuario" runat="server" CssClass="btn btn-link" ForeColor="#28a745" Text="[Usuario]" />
+                            <asp:Label ID="lblUsuario" runat="server" Text=""><i class="fas fa-user" style=" color: #28a745;"></i></asp:Label>
                         </div>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <asp:Button ID="btnUsuario" runat="server" CssClass="btn btn-link" ForeColor="#28a745" Text="[Usuario]" />
-                        <asp:Label ID="lblUsuario" runat="server"  Text=""><i class="fas fa-user" style=" color: #28a745;"></i></asp:Label>
                     </div>
                 </nav>
+            </div>
+            <div id="content">
+                <br />
+                <div id="infoExer">
+                    <div class="row">
+                        <div class="col" id="dt">
+                            <asp:Label ID="Label8" runat="server" Text="Data Início: "></asp:Label>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <asp:TextBox ID="TextBox2" runat="server" TextMode="Date"></asp:TextBox>
+                            <br /><br />
+                            <asp:Label ID="Label9" runat="server" Text="Data Término: "></asp:Label>
+                            &nbsp;
+                            <asp:TextBox ID="TextBox3" runat="server" TextMode="Date"></asp:TextBox>
+                        </div>
+                        <div class="col" id="exc">
+                            <asp:Label ID="Label10" runat="server" Text="Exercício:"></asp:Label>
+                            <br />
+                            <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+                            <br />
+                            <asp:Label ID="Label11" runat="server" Text="Data Início:"></asp:Label>
+                            <br />
+                            <asp:TextBox ID="TextBox4" runat="server" TextMode="Date"></asp:TextBox>
+                            <br />
+                            <asp:Label ID="Label12" runat="server" Text="Data Término:"></asp:Label>
+                            <br />
+                            <asp:TextBox ID="TextBox5" runat="server" TextMode="Date"></asp:TextBox>
+                        </div>
+                        <div class="col" id="parametros">
+                            <div class="row">
+                                <div class="col">
+                                    <asp:Label ID="Label13" runat="server" Text="Quantidade Semanas: "></asp:Label>
+                                </div>
+                                <div class="col">
+                                    <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <asp:Label ID="Label14" runat="server" Text="Duração Fase - (dias): "></asp:Label>
+                                </div>
+                                <div class="col">
+                                    <asp:DropDownList ID="DropDownList3" runat="server"></asp:DropDownList>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <asp:Label ID="Label15" runat="server" Text="Quantidade Treinos x Dia: "></asp:Label>
+                                </div>
+                                <div class="col">
+                                    <asp:DropDownList ID="DropDownList4" runat="server"></asp:DropDownList>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <asp:Label ID="Label16" runat="server" Text="Intervalo Treino x Dia: "></asp:Label>
+                                </div>
+                                <div class="col">
+                                    <asp:DropDownList ID="DropDownList5" runat="server"></asp:DropDownList>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <asp:Label ID="Label17" runat="server" Text="Peso Treino: "></asp:Label>
+                                </div>
+                                <div class="col">
+                                    <asp:DropDownList ID="DropDownList6" runat="server"></asp:DropDownList>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <asp:Label ID="Label18" runat="server" Text="Peso Desafio:"></asp:Label>
+                                </div>
+                                <div class="col">
+                                    <asp:DropDownList ID="DropDownList7" runat="server"></asp:DropDownList>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="observacao">
+                    <br /><br />
+                    <div id="obsLabel">
+                        <asp:Label ID="Label19" runat="server" Text="Observações Clínicas:"></asp:Label>
+                    </div>
+                    <div id="obstext">
+                        <asp:TextBox ID="TextBox6" CssClass="txt-obs" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+                <div id="content_bt_tratamento">
+                    <br />
+                    <asp:Button ID="Button1" runat="server" CssClass="btn btn-primary btn-lg" Width="120px" Text="Salvar" />
+                    <asp:Button ID="Button2" runat="server" CssClass="btn btn-primary btn-lg" Width="120px" BackColor="#1198ff" Text="Voltar" />
+                    <asp:Button ID="Button3" runat="server" CssClass="btn btn-primary btn-lg" Width="140px" Text="Estátisticas" />
+                </div>
             </div>
         </div>
     </form>
--- WebApp/Tratamento.aspx.designer.vb
+++ WebApp/Tratamento.aspx.designer.vb
@@ -41,6 +41,24 @@ Partial Public Class Tratamento
     Protected WithEvents TextBox1 As Global.System.Web.UI.WebControls.TextBox
 
     '''<summary>
+    '''Controle Label4.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label4 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label5.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label5 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
     '''Controle Label2.
     '''</summary>
     '''<remarks>
@@ -59,6 +77,24 @@ Partial Public Class Tratamento
     Protected WithEvents Label3 As Global.System.Web.UI.WebControls.Label
 
     '''<summary>
+    '''Controle Label6.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label6 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label7.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label7 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
     '''Controle btnUsuario.
     '''</summary>
     '''<remarks>
@@ -75,4 +111,247 @@ Partial Public Class Tratamento
     '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
     '''</remarks>
     Protected WithEvents lblUsuario As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle Label8.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label8 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle TextBox2.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents TextBox2 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Controle Label9.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label9 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle TextBox3.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents TextBox3 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Controle Label10.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label10 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList1 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle Label11.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label11 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle TextBox4.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents TextBox4 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Controle Label12.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label12 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle TextBox5.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents TextBox5 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Controle Label13.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label13 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList2.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList2 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle Label14.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label14 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList3.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList3 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle Label15.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label15 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList4.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList4 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle Label16.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label16 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList5.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList5 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle Label17.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label17 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList6.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList6 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle Label18.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label18 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle DropDownList7.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents DropDownList7 As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Controle Label19.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label19 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle TextBox6.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents TextBox6 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Controle Button1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle Button2.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Button2 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle Button3.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Button3 As Global.System.Web.UI.WebControls.Button
 End Class
